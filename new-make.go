cf02c2eb91fab4cfd300ec0c2efda661e6a5ee93
tanweiquan <1274681067@qq.com>
2022-03-21 10:22:58 +0800

    slice_map

--- new-make.go
+++ new-make.go
@@ -3,13 +3,19 @@ package new_make
 import "fmt"
 
 func main() {
-	// 关键字 new 主要用来创建一个引用类型的结构体，只有结构体可以用。
-	// 关键字 make 是用来创建和初始化一个slice或者map。我们可以直接赋值来使用
-	// 直接赋值：
+	// 关键字 new 主要用来给基本类型和结构体开辟内存，返回对应类型的指针。
+	// 关键字 make 是用来给特殊类型（slice、map、chan）开辟内存，返回对应的类型。初始化后我们可以变量赋值。
+	// new
+	a := "hello world"
+	b := new(string)
+	b = &a
+	fmt.Println(b)
+	// make
+	// 有值的初始化
 	e := []int64{1, 2, 3}                 // slice
 	f := map[string]int64{"a": 3, "b": 4} // map
 	fmt.Println(e, f)
-	// 使用make来创建和初始化切片slice和字典map
+	// 无值初始化，使用make来创建和初始化切片slice和字典map
 	s := make([]int64, 5)
 	s1 := make([]int64, 0, 5)
 	m1 := make(map[string]int64, 5)
